1130afebb1fb75b99d0816ab26a18b97e494674e
Shane Randolph <shanerandolph@Shanes-MacBook-Pro.local>
2018-12-15 12:42:34 -0800

    formData client side

--- src/components/pages/show/NewShow.js
+++ src/components/pages/show/NewShow.js
@@ -7,17 +7,21 @@ class NewShow extends Component {
 	constructor(props) {
 		super(props);
 		let editMode = this.props.match.params.showId ? true : false;
+		const formData = new FormData();
+
 		this.state = {
-			showParams: {
-				title: '',
-				description: '',
-				eventURL: '',
-				venueName: '',
-				address: '',
-				videoPreview: '',
-				videoDescription: '',
-				reasonRecommend: ''
-			},
+			// showParams: {
+			// 	title: '',
+			// 	description: '',
+			// 	eventURL: '',
+			// 	venueName: '',
+			// 	address: '',
+			// 	videoPreview: '',
+			// 	videoDescription: '',
+			// 	reasonRecommend: '',
+      //   file: null
+			// },
+			showParams: formData,
 			saving: false,
 			editMode: editMode,
 			ready: !editMode //need to get editable show params still
@@ -27,10 +31,12 @@ class NewShow extends Component {
 		this.handleEventURLUpdate = this.handleEventURLUpdate.bind(this);
 		this.handleVenueNameUpdate = this.handleVenueNameUpdate.bind(this);
 		this.handleAddressUpdate = this.handleAddressUpdate.bind(this);
-		
+
 		this.handleVideoPreviewUpdate = this.handleVideoPreviewUpdate.bind(this);
 		this.handleVideoDescriptionUpdate = this.handleVideoDescriptionUpdate.bind(this);
 		this.handleReasonRecommendUpdate = this.handleReasonRecommendUpdate.bind(this);
+    this.handleImageUpdate = this.handleImageUpdate.bind(this);
+
 
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
@@ -41,7 +47,7 @@ class NewShow extends Component {
 			await axios.get('https://comedyhere-server.herokuapp.com/api/shows/' + _showId, { crossdomain: true }).then(res => {
 				// this.setState({show: res.data})
 				console.log(res.data)
-				this.setState( prevState => ({ showParams: 
+				this.setState( prevState => ({ showParams:
         			{...res.data}
       			}))
       			this.setState({ready: true});
@@ -52,7 +58,7 @@ class NewShow extends Component {
 				this.setState({loadPending: false})
 			})
 		}
-		
+
 	}
 	handleSubmit (e) {
 		e.preventDefault();
@@ -86,51 +92,74 @@ class NewShow extends Component {
 	}
 	handleTitleUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, title: value}
-      	}))
+		this.updateFormData('title', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, title: value}
+    //   	}))
 	}
 	handleDescriptionUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, description: value}
-      	}))
+		this.updateFormData('description', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, description: value}
+    //   	}))
 	}
 	handleEventURLUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, eventURL: value}
-      	}))
+		this.updateFormData('eventURL', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, eventURL: value}
+    //   	}))
 	}
 	handleVenueNameUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, venueName: value}
-      	}))
+		this.updateFormData('venueName', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, venueName: value}
+    //   	}))
 	}
 	handleAddressUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, address: value}
-      	}))
+		this.updateFormData('address', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, address: value}
+    //   	}))
 	}
 	handleVideoPreviewUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, videoPreview: value}
-      	}))
+		this.updateFormData('videoPreview', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, videoPreview: value}
+    //   	}))
 	}
 	handleVideoDescriptionUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, videoDescription: value}
-      	}))
+		this.updateFormData('videoDescription', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, videoDescription: value}
+    //   	}))
 	}
 	handleReasonRecommendUpdate(e) {
 		let value = e.target.value;
-		this.setState( prevState => ({ showParams : 
-        	{...prevState.showParams, reasonRecommend: value}
-      	}))
+		this.updateFormData('reasonRecommend', value);
+		// this.setState( prevState => ({ showParams :
+    //     	{...prevState.showParams, reasonRecommend: value}
+    //   	}))
+	}
+
+  handleImageUpdate(e) {
+    let value = e.target.files[0];
+		this.updateFormData('file', value);
+    // this.setState( prevState => ({ showParams :
+    //       {...prevState.showParams, file: value}
+    //     }))
+  }
+	updateFormData(key, value) {
+		let formData = this.state.showParams;
+		formData.set(key, value);
+		this.setState( { showParams: formData }, () => {
+			alert(this.state.showParams.get(key))
+		} );
 	}
 	render() {
 		return (
@@ -138,41 +167,41 @@ class NewShow extends Component {
 				<div className="show-page">
 					<header>New Show</header>
 					{
-						this.state.ready ? 
+						this.state.ready ?
 						<form onSubmit={this.handleSubmit}>
 							<Input
 								title={'Title'}
 								name={'title'}
 								type={'text'}
-								value={this.state.showParams.title || ''}
+								// value={this.state.showParams.title || ''}
 								handleChange={this.handleTitleUpdate}
 							/>
 							<Input
 								title={'Description'}
 								name={'description'}
 								type={'text'}
-								value={this.state.showParams.description || ''}
+								// value={this.state.showParams.description || ''}
 								handleChange={this.handleDescriptionUpdate}
 							/>
 							<Input
 								title={'Event URL'}
 								name={'eventURL'}
 								type={'text'}
-								value={this.state.showParams.eventURL || ''}
+								// value={this.state.showParams.eventURL || ''}
 								handleChange={this.handleEventURLUpdate}
 							/>
 							<Input
 								title={'Venue Name'}
 								name={'venueName'}
 								type={'text'}
-								value={this.state.showParams.venueName || ''}
+								// value={this.state.showParams.venueName || ''}
 								handleChange={this.handleVenueNameUpdate}
 							/>
 							<Input
 								title={'Venue Address'}
 								name={'address'}
 								type={'text'}
-								value={this.state.showParams.address || ''}
+								// value={this.state.showParams.address || ''}
 								handleChange={this.handleAddressUpdate}
 							/>
 
@@ -180,23 +209,31 @@ class NewShow extends Component {
 								title={'Video Preview'}
 								name={'videoPreview'}
 								type={'text'}
-								value={this.state.showParams.videoPreview || ''}
+								// value={this.state.showParams.videoPreview || ''}
 								handleChange={this.handleVideoPreviewUpdate}
 							/>
 							<Input
 								title={'Video Description'}
 								name={'videoDescription'}
 								type={'text'}
-								value={this.state.showParams.videoDescription || ''}
+								// value={this.state.showParams.videoDescription || ''}
 								handleChange={this.handleVideoDescriptionUpdate}
 							/>
 							<Input
 								title={'Reason We Recommend'}
 								name={'reasonRecommend'}
 								type={'text'}
-								value={this.state.showParams.reasonRecommend || ''}
+								// value={this.state.showParams.reasonRecommend || ''}
 								handleChange={this.handleReasonRecommendUpdate}
 							/>
+
+              <Input
+                title={'Image'}
+                name={'file'}
+                type={'file'}
+                // value={''}
+                handleChange={this.handleImageUpdate}
+              />
 							<button className="btn" onClick={this.handleSubmit}>
 								{
 									this.state.editMode ? 'Update Show' : 'Add Show'
